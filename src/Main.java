b2e5061967ef8ae3c08b454d7762fe2fe0577035
VivianaMagali <magi61012@gmail.com>
2017-10-06 11:20:57 +0100

    version con forech y uso de  operador ternario

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,8 @@ public class Main {
 
         Map<Integer,Integer> histogram=new HashMap<>();
 
-        for (int i = 0; i <data.length ; i++) {
-            if(!histogram.containsKey(data[i])){
-                histogram.put(data[i], 1);
-            }else{
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            }
+        for (int value: data) {
+            histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
         }
 
         for (int key: histogram.keySet()) {
